a142b90df49b06f88adbd0ca8d1b203c4fc0a4fa
오준솔 <>
2022-09-22 07:10:19 +0900

    mid save

--- GifGram/GifGram/Scene/Home/HomeViewController.swift
+++ GifGram/GifGram/Scene/Home/HomeViewController.swift
@@ -9,33 +9,21 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    var pageController: UIPageViewController!
-    var viewControllers = NSMutableArray()
-    var currentPageIndex : Int!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let firstViewController = storyboard.instantiateViewController(withIdentifier: "trendPage")
-        firstViewController.view.tag = 0
-        viewControllers.add(firstViewController)
-        
-        let secondViewController = storyboard.instantiateViewController(withIdentifier: "searchPage")
-        secondViewController.view.tag = 1
-        viewControllers.add(secondViewController)
-        
-        pageController = UIPageViewController(transitionStyle: .scroll, navigationOrientation:.horizontal, options: nil)
-        
-        pageController.view.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height - (self.tabBarController?.tabBar.frame.size.height ?? 0));
-        
-        currentPageIndex = 0
-        pageController.setViewControllers([viewControllers.object(at: currentPageIndex) as! UIViewController], direction: .forward, animated: false) {(isFinished:Bool) -> Void in
-            
-        }
-        
-        self.addChild(pageController)
-        self.view.addSubview(pageController.view)
+        searchBar.delegate = self
     }
 
+
+}
+
+extension HomeViewController: UISearchBarDelegate {
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.definesPresentationContext = true
+        self.performSegue(withIdentifier: "moveToSearch", sender: nil)
+    }
 }
